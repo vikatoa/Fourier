8380f71fe343d04a4dd09e351812b88d04c7ea9a
vikatoa <victor@sevel.pro>
2025-10-07 11:12:32 +0200

    a new main

--- main.cpp
+++ main.cpp
@@ -6,25 +6,27 @@
 
 int main()
 {
-   // Interface interface;
-    Fourier image;
-    image.load_image("data/capture.jpg"); 
-    sf::Image i1 = image.app_kernel(image.image, {{1, 1, 1}, {0, 0, 0}, {-1, -1, -1}});
-    sf::Image i2 = image.app_kernel(image.image, {{1, 0, -1}, {1, 0, -1}, {1, 0, -1}});
-    sf::Image i  = image.add_images(i1, i2);
-    sf::Image w  = image.WnB(i, 60);
+   Interface interface;
+    // Fourier image;
+    // image.load_image("data/capture.jpg"); 
+    // sf::Image i1 = image.app_kernel(image.image, {{1, 1, 1}, {0, 0, 0}, {-1, -1, -1}});
+    // sf::Image i2 = image.app_kernel(image.image, {{1, 0, -1}, {1, 0, -1}, {1, 0, -1}});
+    // sf::Image i  = image.add_images(i1, i2);
+    // sf::Image w  = image.WnB(i, 60);
     // image.minimal_display(image.image);
     // image.minimal_display(i1);
     // image.minimal_display(i2);
     // image.minimal_display(i);
-    image.minimal_display(w);
+    // image.minimal_display(w);
 
-    /*
+    
     while(interface.isOpen()){
-        interface.events();
-        interface.show();
+        if(interface.timeToFrame()){
+            interface.events();
+            interface.show();
+        }
         interface.update();
     }
-    */
+    
     return 0;
 }
